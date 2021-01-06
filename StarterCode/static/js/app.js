5648a9870c6e8d64b0dffbcc63d36f00eff8cd30
Isabelle Sanford <isabellesanford@gmail.com>
2021-01-05 23:12:48 -0800

    fix bugs to filter right on enter

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -9,25 +9,11 @@ var button = d3.select("#filter-btn");
 button.on("click", runEnter);
 form.on("submit", runEnter);
 
-// function showTable(filteredData) {
-
-//     filteredData.forEach(function(sighting) {
-//         //var tbod = d3.select(".tbod");
-//         tbody.html("");
-
-//         var row = tbody.append("tr");
-//         Object.entries(sighting).forEach(function([key, value]) {
-//             var cell = row.append("td");
-//             cell.text(value);
-//         })
-
-// }) };
-
-// YOUR CODE HERE!
-
 
 
 function runEnter() {
+
+    d3.event.preventDefault();
 
     var inputElement = d3.select("#datetime");
     var inputValue = inputElement.property("value");
@@ -35,16 +21,12 @@ function runEnter() {
     
     var filtered = data.filter(account => account.datetime === inputValue);
     
-    if (filtered.length < 1) {filtered = data;};
+    //if (filtered.length < 1) {filtered = data;};
 
     tbody.html("");
-    
-    // showTable(filtered);
+
 
     filtered.forEach(function(sighting) {
-        //var tbod = d3.select(".tbod");
-        console.log(sighting);
-
         var row = tbody.append("tr");
         Object.entries(sighting).forEach(function([key, value]) {
             var cell = row.append("td");
@@ -55,16 +37,12 @@ function runEnter() {
 
 };
 
-runEnter();
 
-// data.forEach(function(weatherReport) {
-//   console.log(weatherReport);
-//   var row = tbody.append("tr");
-//   Object.entries(weatherReport).forEach(function([key, value]) {
-//     console.log(key, value);
-//     // Append a cell to the row for each value
-//     // in the weather report object
-//     var cell = row.append("td");
-//     cell.text(value);
-//   });
-// });
+// initial setup
+data.forEach(function(sighting) {
+    var row = tbody.append("tr");
+    Object.entries(sighting).forEach(function([key, value]) {
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
